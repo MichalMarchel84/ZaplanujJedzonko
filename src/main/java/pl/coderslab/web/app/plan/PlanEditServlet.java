efba5058c2780a443040f569a7f85a037abdc2dd
MichalMarchel84 <michal.marchel84@gmail.com>
2021-05-05 16:44:50 +0200

    Front-end for improved plan edition

--- src/main/java/pl/coderslab/web/app/plan/PlanEditServlet.java
+++ src/main/java/pl/coderslab/web/app/plan/PlanEditServlet.java
@@ -1,12 +1,15 @@
 package pl.coderslab.web.app.plan;
 
 import pl.coderslab.dao.PlanDao;
+import pl.coderslab.dao.RecipeDAO;
 import pl.coderslab.model.Plan;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
 
 @WebServlet(name = "PlanEditServlet", value = "/app/plan/edit")
 public class PlanEditServlet extends HttpServlet {
@@ -14,6 +17,8 @@ public class PlanEditServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Plan plan = (new PlanDao()).getById(Integer.parseInt(request.getParameter("id")));
         request.setAttribute("plan", plan);
+        request.setAttribute("details", plan.getDetails());
+        request.setAttribute("recipes", (new RecipeDAO()).findAllByAdmin((Integer)request.getSession().getAttribute("adminId")));
         request.setAttribute("component", "/app/plan/edit.jsp");
         getServletContext().getRequestDispatcher("/app/frame.jsp").forward(request, response);
     }
@@ -23,6 +28,9 @@ public class PlanEditServlet extends HttpServlet {
         Plan plan = (new PlanDao()).getById(Integer.parseInt(request.getParameter("id")));
         plan.setName(request.getParameter("name"));
         plan.setDescription(request.getParameter("description"));
+        for (Map.Entry<String, String[]> e : request.getParameterMap().entrySet()){
+            System.out.println(e.getKey() + " : " + Arrays.toString(e.getValue()));
+        }
         if((new PlanDao()).updatePlan(plan) > 0) response.sendRedirect("/app/plan/list");
         else response.getWriter().append("Something went wrong...");
     }
